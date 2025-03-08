1a87d9b384d76e6d6ae47de4d24aaa3267d9bcf0
shuang <1554096735@qq.com>
2025-03-08 22:42:01 +0800

    添加logback日志 测试日志功能

--- shane-web/src/main/java/com/shane/web/TestController.java
+++ shane-web/src/main/java/com/shane/web/TestController.java
@@ -1,16 +1,18 @@
 package com.shane.web;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @RequestMapping("/healthcheck")
     private String healthCheck(){
+        log.info("healthcheck!");
         return "healthcheck!";
     }
-
 
 }
